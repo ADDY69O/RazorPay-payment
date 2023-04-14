01b048c2be26fdc80c9278ac124dd2a3004ecea5
Aditya <kohsa9211420@gmail.com>
2023-04-14 16:15:19 +0530

    aaa

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,9 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 connectDb();
+app.get('/',(req,res)=>{
+    res.send("backend ")
+})
 app.use("/api",route);
 
 app.get("/api/getKey",(req,res)=>{
